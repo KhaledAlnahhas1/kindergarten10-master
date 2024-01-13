f876dc7b16a3764dab420d18adc0f0e47f2e2cf8
Khaled <2210859032@fh-burgenland.at>
2024-01-13 10:29:15 +0100

    Dialog

---
+++ kindergarten10-master/src/app/shared/dialog.ts
@@ -0,0 +1,21 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-dialog',
+  template: `
+    <h1 mat-dialog-title>{{ data.title }}</h1>
+    <div mat-dialog-content>
+      <p>{{ data.message }}</p>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button [mat-dialog-close]="true">OK</button>
+    </div>
+  `,
+})
+export class DialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string }
+  ) {}
+}
